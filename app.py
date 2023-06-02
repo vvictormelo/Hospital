d401d843fda614f6b7e42548e3e27c398a7283c7
Victor Melo <melovictor515@gmail.com>
2023-06-02 02:27:42 -0300

    Incremento dos relatórios versão 1

--- app.py
+++ app.py
@@ -165,14 +165,23 @@ class menufront:
         print("6.2 - Vincular Hospital a Enfermeira(o)")
         print("6.3 - Vincular Médico(a) a Paciente")
         print("")
-        print("7 - Sair")
+        print("Relatórios")
+        print("7 - Relatório geral de médicos")
+        print("7.1 - Relatório geral de pacientes")
+        print("7.2 - Relatório geral de enfermeiros")
+        print("7.3 - Relatório geral de hospitais")
+        print("7.4 - Relatório de pacientes residentes no Centro de Aracaju")
+        print("7.5 - Relatório médicos e telefones")
+        print("7.6 - Relatório do corpo clínico por hospital")
+        print("")
+        print("8 - Sair")
 
     def program(self):
         print("\033[1;36m\nSEJA BEM VINDO...\033[m")
 
         self.acao = 0
 
-        while self.acao != 7:
+        while self.acao != 8:
             try:
                 self.menu()
                 self.acao = float(input("\nInsira a ação desejada: "))
@@ -212,9 +221,23 @@ class menufront:
                     case 6.3:
                         self.vincular_med_pac()
                     case 7:
-                        self.acao = 7
+                        self.rel_geral_medicos()
+                    case 7.1:
+                        self.rel_geral_pacientes()
+                    case 7.2:
+                        self.rel_geral_enfermeiros()
+                    case 7.3:
+                        self.rel_geral_hospital()
+                    case 7.4:
+                        self.rel_paciente_centro_aju()
+                    case 7.5:
+                        self.rel_medicos_telefone()
+                    case 7.6:
+                        self.rel_corpo_clinico()
+                    case 8:
+                        self.acao = 8
                         print("\nSaindo... Até logo!")
-                if self.acao > 7 or self.acao < 1:
+                if self.acao > 8 or self.acao < 1:
                     print("\033[1;31m\nOops...Ação inválida. Tente novamente!\n\033[m")
 
             except ValueError:
@@ -528,6 +551,175 @@ class menufront:
 
         med_enf = entities.MedEnf(crm, coren)
         med_enf.vinc_medicos_enfermeiros()
+
+    # Métodos dos relatórios
+    def rel_geral_medicos(self):
+        print("\033[1;36m-=\033[m" * 15)
+        print(f'\033[1;36m{"RELATÓRIO GERAL DE MÉDICOS":>30}\033[m')
+        print("\033[1;36m-=\033[m" * 15)
+
+        query = "select crm, cpf, nome, rua, bairro, cidade, cep from medico"
+
+        try:
+            db.conectadb()
+            db.cursor.execute(query)
+
+            print("\nMédicos cadastados:")
+            for medico in db.cursor.fetchall():
+                crm, cpf, nome, rua, bairro, cidade, cep = medico
+                print(
+                    f"""CRM: {crm} / CPF: {cpf} / Nome do Médico: {nome} / Endereço. Rua: {rua} / Bairro: {bairro} / Cidade: {cidade} / CEP: {cep}"""
+                )
+
+        except Exception as e:
+            print("Erro ao buscar médicos:", e)
+
+        finally:
+            db.desconectdb()
+
+    def rel_geral_pacientes(self):
+        print("\033[1;36m-=\033[m" * 15)
+        print(f'\033[1;36m{"RELATÓRIO GERAL DE PACIENTES":>30}\033[m')
+        print("\033[1;36m-=\033[m" * 15)
+
+        query = "select cpf, rg, nome, rua, bairro, cidade, cep from paciente"
+
+        try:
+            db.conectadb()
+            db.cursor.execute(query)
+
+            print("\nPacientes cadastados:")
+            for paciente in db.cursor.fetchall():
+                cpf, rg, nome, rua, bairro, cidade, cep = paciente
+                print(
+                    f"""CPF: {cpf} / RG: {rg} / Nome do Paciente: {nome} / Endereço. Rua: {rua} / Bairro: {bairro} / Cidade: {cidade} / CEP: {cep}"""
+                )
+
+        except Exception as e:
+            print("Erro ao buscar pacientes:", e)
+
+        finally:
+            db.desconectdb()
+
+    def rel_geral_enfermeiros(self):
+        print("\033[1;36m-=\033[m" * 15)
+        print(f'\033[1;36m{"RELATÓRIO GERAL DE ENFERMEIROS":>30}\033[m')
+        print("\033[1;36m-=\033[m" * 15)
+
+        query = "select coren, cpf, nome, rua, bairro, cidade, cep from enfermeira"
+
+        try:
+            db.conectadb()
+            db.cursor.execute(query)
+
+            print("\nEnfermeiros cadastados:")
+            for enfermeiro in db.cursor.fetchall():
+                coren, cpf, nome, rua, bairro, cidade, cep = enfermeiro
+                print(
+                    f"""COREN: {coren} / CPF: {cpf} / Nome do Enfermeiro: {nome} / Endereço. Rua: {rua} / Bairro: {bairro} / Cidade: {cidade} / CEP: {cep}"""
+                )
+
+        except Exception as e:
+            print("Erro ao buscar enfermeiros:", e)
+
+        finally:
+            db.desconectdb()
+
+    def rel_geral_hospital(self):
+        print("\033[1;36m-=\033[m" * 15)
+        print(f'\033[1;36m{"RELATÓRIO GERAL DE HOSPITAIS":>30}\033[m')
+        print("\033[1;36m-=\033[m" * 15)
+
+        query = "select cnpj, nome, rua, bairro, cidade, cep, telefone from hospital"
+
+        try:
+            db.conectadb()
+            db.cursor.execute(query)
+
+            print("\nHospitais cadastados:")
+            for hospital in db.cursor.fetchall():
+                cnpj, nome, rua, bairro, cidade, cep, telefone = hospital
+                print(
+                    f"""CNPJ: {cnpj} / Nome do Hospital: {nome} / Endereço. Rua: {rua} / Bairro: {bairro} / Cidade: {cidade} / CEP: {cep} / Telefone: {telefone}"""
+                )
+
+        except Exception as e:
+            print("Erro ao buscar hospital:", e)
+
+        finally:
+            db.desconectdb()
+
+    def rel_paciente_centro_aju(self):
+        print("\033[1;36m-=\033[m" * 15)
+        print(f'\033[1;36m{"RELATÓRIO PACIENTE NO CENTRO DE AJU":>30}\033[m')
+        print("\033[1;36m-=\033[m" * 15)
+
+        query = "select cpf, rg, nome, rua, bairro, cidade, cep from paciente where UPPER(bairro) = 'CENTRO' and UPPER(cidade) = 'ARACAJU' "
+
+        try:
+            db.conectadb()
+            db.cursor.execute(query)
+
+            print("\nPacientes cadastados:")
+            for paciente in db.cursor.fetchall():
+                cpf, rg, nome, rua, bairro, cidade, cep = paciente
+                print(
+                    f"""CPF: {cpf} / RG: {rg} /Nome do paciente: {nome} / Endereço. Rua: {rua} / Bairro: {bairro} / Cidade: {cidade} / CEP: {cep}"""
+                )
+
+        except Exception as e:
+            print("Erro ao buscar paciente:", e)
+
+        finally:
+            db.desconectdb()
+
+    def rel_medicos_telefone(self):
+        print("\033[1;36m-=\033[m" * 15)
+        print(f'\033[1;36m{"RELATÓRIO MÉDICOS COM TELEFONE":>30}\033[m')
+        print("\033[1;36m-=\033[m" * 15)
+
+        query = "select crm, cpf, nome, tel from medico m join telefone t on t.documento = m.crm order by m.crm"
+
+        try:
+            db.conectadb()
+            db.cursor.execute(query)
+
+            print("\nMédicos cadastados:")
+            for medico in db.cursor.fetchall():
+                crm, cpf, nome, tel = medico
+                print(
+                    f"""CRM: {crm} / CPF: {cpf} / Nome do medico: {nome} / Telefone: {tel}"""
+                )
+
+        except Exception as e:
+            print("Erro ao buscar medicos:", e)
+
+        finally:
+            db.desconectdb()
+
+    def rel_corpo_clinico(self):
+        print("\033[1;36m-=\033[m" * 15)
+        print(f'\033[1;36m{"RELATÓRIO CORPO CLÍNICO":>30}\033[m')
+        print("\033[1;36m-=\033[m" * 15)
+
+        query = "select h.cnpj, h.nome, m.crm, m.nome, e.coren, e.nome from hospital h join hosp_med hm on h.cnpj = hm.cnpj join  medico m on m.crm = hm.crm join hosp_enfer he on he.cnpj = h.cnpj join enfermeira e on e.coren = he.coren order by h.cnpj"
+
+        try:
+            db.conectadb()
+            db.cursor.execute(query)
+
+            print("\Corpo Clínico cadastado:")
+            for corpo in db.cursor.fetchall():
+                h_cnpj, h_nome, m_crm, m_nome, e_coren, e_nome = corpo
+                print(
+                    f"""Hospital - CNPJ: {h_cnpj} / Hospital - Nome: {h_nome} / Médico - CRM: {m_crm} / Médico - Nome: {m_nome} / Enfermério - COREN: {e_coren} / Enfermério - Nome: {e_nome}"""
+                )
+
+        except Exception as e:
+            print("Erro ao buscar corpo clínico:", e)
+
+        finally:
+            db.desconectdb()
 
 
 if __name__ == "__main__":
